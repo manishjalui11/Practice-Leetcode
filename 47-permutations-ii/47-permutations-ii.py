cacb2ee33986a0b4e5d34135267e94e5c5acafb7
Manish Jalui <68913884+manishjalui11@users.noreply.github.com>
2022-08-09 02:23:07 +0530

    Time: 91 ms (63.60%), Space: 14.2 MB (78.15%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [[nums[0]]]
+        
+        res = self.permuteUnique(nums[1:])
+
+        for i in range(len(res)-1, -1 , -1):
+            j = 0
+            while j < len(res[i]):
+                if res[i][j] == nums[0]: #to account for repeated nums
+                    break
+                lst = res[i][:]
+                lst.insert(j, nums[0])
+                res.append(lst)
+                j += 1
+				
+            res[i].insert(j,nums[0])
+                
+        return res
