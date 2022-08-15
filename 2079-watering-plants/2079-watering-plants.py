d23996421d955e045209da9517c774d19607050f
Manish Jalui <68913884+manishjalui11@users.noreply.github.com>
2022-08-16 01:10:29 +0530

    Time: 55 ms (87.79%), Space: 13.9 MB (83.97%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        buck = capacity                      
+        i = 0
+        steps = 0
+        while(i < len(plants)):
+            steps += 1                     
+            if(buck >= plants[i]):          
+                buck-= plants[i]                  
+            else:
+                buck = capacity           
+                buck-= plants[i]   
+                steps += i+i             
+            i += 1
+                
+                
+        return steps
